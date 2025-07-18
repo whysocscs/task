821693949b98b5c4327cee2f35eea0c1d8679b16
whysocscs <kudo119@naver.com>
2025-07-18 05:51:21 -0400

    task1 on sum_nbo branch new

--- sum-nbo.c
+++ sum-nbo.c
@@ -3,27 +3,41 @@
 #include<stdint.h>
 #include<stdlib.h>
 
+
+uint32_t filereadvalue(char * Filename){
+	FILE *fp;
+	uint32_t num1 =0;
+	for(int i=0; i<=3; i++){
+		if((fp = fopen(Filename, "rb")) == NULL){
+			printf("fopen error");
+			exit(1);
+		}
+		fseek(fp, 0, SEEK_END);
+		if( ftell(fp) < 4 ){
+			printf(" ftell error");
+			exit(1);
+		}
+
+		fseek(fp, 0, SEEK_SET);
+		fread(&num1, sizeof(num1), 1, fp);
+		fclose(fp);
+		return ntohl(num1);
+	}
+}
+
+
 int main(int argc, char *argv[]){
 	uint32_t hap =0;
 	uint32_t num2=0;
-	FILE *fp= NULL;
 	for(int i=1; i<=3;i++){
-		if((fp = fopen(argv[i], "rb"))==NULL){
-			printf("error");
-			exit(1);
-		}
-		fread(&num2 , sizeof(num2), 1, fp);
-		num2 = ntohl(num2);
+		num2 = filereadvalue(argv[i]);
 		printf("%d(0x%08x)", num2,num2);
 		hap += num2;
 		if(i+1!=4)
-			printf(" + ");
-		else{
-		
-			printf("= %d(0x%08x)", hap, hap);		
+			printf(" + ");		
 		}
-		fclose(fp);
-	}
+	printf("= %d(0x%08x)", hap, hap);	
 }
+
 
 
